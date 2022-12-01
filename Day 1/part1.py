5c3764e1ca258c5abf661c2010aaf855fdfdf4cb
Jin <weijin.tan@kingmakergames.co>
2022-12-01 14:28:14 +0800

    Day 1

---
+++ Day 1/part1.py
@@ -0,0 +1,25 @@
+'''
+This list represents the Calories of the food carried by five Elves:
+
+The first Elf is carrying food with 1000, 2000, and 3000 Calories, a total of 6000 Calories.
+The second Elf is carrying one food item with 4000 Calories.
+The third Elf is carrying food with 5000 and 6000 Calories, a total of 11000 Calories.
+The fourth Elf is carrying food with 7000, 8000, and 9000 Calories, a total of 24000 Calories.
+The fifth Elf is carrying one food item with 10000 Calories.
+In case the Elves get hungry and need extra snacks, they need to know which Elf to ask: they'd like to know how many Calories are being carried by the Elf carrying the most Calories. In the example above, this is 24000 (carried by the fourth Elf).
+
+Find the Elf carrying the most Calories. How many total Calories is that Elf carrying?
+'''
+
+
+file = open("input.txt", "r")
+totalCalories = file.read()
+runningCount = 0
+maxCalories = 0
+for cal in totalCalories.split('\n'):
+  if not cal:
+    maxCalories = max(maxCalories, runningCount)
+    runningCount = 0
+  else:
+    runningCount += int(cal)
+print(maxCalories)
