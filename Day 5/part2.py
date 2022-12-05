9c6c514e2bfe2a5efcdaf21fd80e65ca422f6bc2
Jin <weijin.tan@kingmakergames.co>
2022-12-05 13:24:30 +0800

    Day 5

---
+++ Day 5/part2.py
@@ -0,0 +1,18 @@
+# https://adventofcode.com/2022/day/5
+
+file1 = open("crates_transposed.txt", "r")
+results = file1.read()
+crates = results.strip().split('\n')
+
+file2 = open("instructions.txt", "r")
+results = file2.read()
+instructions = results.strip().split('\n')
+simplified_instructions = list(map(lambda x: [int(i) for i in x.split(' ') if i.isnumeric()], instructions))
+
+for inst in simplified_instructions:
+  fromCrate = crates[inst[1]-1]
+  toCrate = crates[inst[2]-1]
+  crates[inst[1]-1] = fromCrate[:len(fromCrate)-inst[0]]
+  crates[inst[2]-1] = toCrate + fromCrate[len(fromCrate)-inst[0]:]
+
+print(''.join([crate[-1:] for crate in crates]))
