1a91c8e71ab746b42613f9e48a160311be05904a
Jin <weijin.tan@kingmakergames.co>
2022-12-10 23:37:13 +0800

    Day 10

---
+++ Day 10/part1.py
@@ -0,0 +1,20 @@
+# https://adventofcode.com/2022/day/9
+
+
+with open("input.txt", "r") as file:
+  input = file.read()
+  signals = input.strip().split('\n')
+
+strength = [0]
+for signal in signals:
+  if signal == 'noop':
+    strength.append(0)
+  else:
+    strength.append(0)
+    strength.append(int(signal.split(' ')[1]))
+
+totalSignalStrength = 0
+for i in range(20, 260, 40):
+  signalStrength = (sum(strength[:i])+1) * i
+  totalSignalStrength += signalStrength
+print(totalSignalStrength, '\n')
---
+++ Day 10/part2.py
@@ -0,0 +1,29 @@
+# https://adventofcode.com/2022/day/9
+
+
+with open("input.txt", "r") as file:
+  input = file.read()
+  signals = input.strip().split('\n')
+
+strength = [1]
+total = 1
+for signal in signals:
+  if signal == 'noop':
+    strength.append(total)
+  else:
+    strength.append(total)
+    total += int(signal.split(' ')[1])
+    strength.append(total)
+
+
+crt = []
+for i in range(len(strength)):
+  currentValue = strength[i]
+  signalRange = [currentValue-1, currentValue, currentValue+1]
+  if i%40 in signalRange:
+    crt.append('#')
+  else:
+    crt.append('.')
+
+for i in range(0, len(crt)-1, 40):
+  print(''.join(crt[i:i+40]))
